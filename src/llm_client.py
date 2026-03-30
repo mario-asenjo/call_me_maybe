3e6c78aa1975c03ba80edd7539b770640a96ff10
Mario Asenjo Pérez <mario.asenjo@outlook.es>
2026-03-30 23:39:42 +0200

    Initial commit with base structure

---
+++ src/llm_client.py
@@ -0,0 +1,19 @@
+"""
+Minimal adapter for real SDK.
+"""
+
+
+def encode_text() -> None:
+    pass
+
+
+def decode_tokens() -> None:
+    pass
+
+
+def next_token_logits() -> None:
+    pass
+
+
+def load_vocab() -> None:
+    pass
---
+++ src/models.py
@@ -0,0 +1,23 @@
+"""
+
+"""
+
+
+class FunctionParameterSpec:
+    """"""
+    pass
+
+
+class FunctionDefinition:
+    """"""
+    pass
+
+
+class PromptItem:
+    """"""
+    pass
+
+
+class FunctionCallResult:
+    """"""
+    pass
